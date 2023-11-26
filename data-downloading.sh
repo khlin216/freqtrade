e1dfc455d6149c77e6514eb8e34a0ed8cc2619bf
khlin <khlincode@gmail.com>
2023-11-26 17:04:52 +0800

    for data downloading

---
+++ data-downloading.sh
@@ -0,0 +1,1 @@
+freqtrade download-data --exchange binance --pairs .*/USDT -t 2h --timerange 20180101-
---
+++ plot-profit.sh
@@ -0,0 +1,1 @@
+freqtrade plot-profit --strategy HullSuiteStrategy_v2 --timeframe '2h' --timerange 20230101-
