adff8f15e4d7e6ffaeafe7ffd216e9f73ba33bfb
khlin <khlincode@gmail.com>
2023-12-03 12:14:44 +0800

    backtest

--- backtesting.sh
+++ backtesting.sh
@@ -1,1 +1,3 @@
-freqtrade backtesting --strategy HullSuiteStrategy_v3 --timerange=20220101-20231212
+# freqtrade backtesting --strategy HullSuiteStrategy_v3 --timerange=20220101-20231212
+# freqtrade backtesting --strategy BB_RPB_TSLmeneguzzo --timerange=20200101-20231212
+freqtrade backtesting --config user_data/config_turbov8.json --strategy turbov8 --timerange=20200101-20231212
