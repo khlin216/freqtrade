f94df5a882353d3eee5a091e3061f38385ea9395
khlin <khlincode@gmail.com>
2023-12-10 12:39:41 +0800

    download and json file

--- data-downloading.sh
+++ data-downloading.sh
@@ -1,1 +1,8 @@
-freqtrade download-data --exchange binance --pairs .*/USDT -t 2h --timerange 20180101-
+# freqtrade download-data --exchange binance --pairs .*/USDT -t 1m --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 5m --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 15m --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 1h --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 4h --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 8h --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 12h --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 1d --timerange 20180101-
