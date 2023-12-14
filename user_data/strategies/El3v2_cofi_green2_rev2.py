3b634a0303136efd8571859dbffdb8397d87cdea
khlin <khlincode@gmail.com>
2023-12-14 08:35:22 +0800

    new python file

--- user_data/strategies/El3v2_cofi_green2_rev2.py
+++ user_data/strategies/El3v2_cofi_green2_rev2.py
@@ -12,6 +12,7 @@ import datetime
 from technical.util import resample_to_interval, resampled_merge
 from datetime import datetime, timedelta
 from freqtrade.persistence import Trade
+
 from freqtrade.strategy import (
     stoploss_from_open,
     merge_informative_pair,
@@ -19,10 +20,57 @@ from freqtrade.strategy import (
     IntParameter,
     CategoricalParameter,
 )
+
 import technical.indicators as ftt
 import math
 import logging
 
 logger = logging.getLogger(__name__)
 
-# @Rallipanos # changes by IcHiAT
+
+    buy_params = {
+        #############
+        # Enable/Disable conditions
+        "buy_condition_1_enable": True,
+        "buy_condition_2_enable": True,
+        "buy_condition_3_enable": True,
+        "buy_condition_4_enable": True,
+        "buy_condition_5_enable": True,
+        "buy_condition_6_enable": True,
+        "buy_condition_7_enable": True,
+        "buy_condition_8_enable": True,
+        "buy_condition_9_enable": True,
+        "buy_condition_10_enable": True,
+        "buy_condition_11_enable": True,
+        "buy_condition_12_enable": True,
+        "buy_condition_13_enable": True,
+        "buy_condition_14_enable": True,
+        "buy_condition_15_enable": True,
+        "buy_condition_16_enable": True,
+        "buy_condition_17_enable": True,
+        "buy_condition_18_enable": True,
+        "buy_condition_19_enable": True,
+        "buy_condition_20_enable": True,
+        "buy_condition_21_enable": True,
+        "buy_condition_22_enable": True,
+        "buy_condition_23_enable": True,
+        "buy_condition_24_enable": True,
+        "buy_condition_25_enable": True,
+        "buy_condition_26_enable": True,
+        #############
+    }
+
+    sell_params = {
+        #############
+        # Enable/Disable conditions
+        "sell_condition_1_enable": True,
+        "sell_condition_2_enable": True,
+        "sell_condition_3_enable": True,
+        "sell_condition_4_enable": True,
+        "sell_condition_5_enable": True,
+        "sell_condition_6_enable": True,
+        "sell_condition_7_enable": True,
+        "sell_condition_8_enable": True,
+        #############
+    }
+
