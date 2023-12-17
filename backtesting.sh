d960527b078df3f261c6dd642af4d70dec1638bb
khlin <khlincode@gmail.com>
2023-12-17 13:27:29 +0800

    backtest

--- backtesting.sh
+++ backtesting.sh
@@ -11,18 +11,18 @@
 # Tested ,Good
 # freqtrade backtesting --config user_data/config_ClucHAnix_5m_4.json --strategy ClucHAnix_5m --timerange=20210101-20221231
 # Tested ,very Good
-# freqtrade backtesting --config user_data/config_ElliotV5_SMA.json --strategy ElliotV5_SMA --timerange=20220101-20231130
+# freqtrade backtesting --config user_data/config_ElliotV5_SMA.json --strategy ElliotV5_SMA --timerange=20231001-20231130
 # Tested , very good
 # freqtrade backtesting --config user_data/config_RalliV1_2.json --strategy RalliV1 --timerange=20231001-20231130
 # Tested , very good
-freqtrade backtesting --config user_data/config_El3v2_tag_cofi_green_2.json --strategy EI3v2_tag_cofi_green --timerange=20220101-20231130
+freqtrade backtesting --config user_data/config_El3v2_tag_cofi_green_2.json --strategy EI3v2_tag_cofi_green --timerange=20200101-20231130
 # Tested , very good
-# freqtrade backtesting --config user_data/config_NASOSv7.json --strategy NASOSv7 --timerange=20220101-20231130
+# freqtrade backtesting --config user_data/config_NASOSv7.json --strategy NASOSv7 --timerange=20200101-20231130
 # Tested , Bad
 # freqtrade backtesting --config user_data/config_min5v5.json --strategy min5v5 --timerange=20210101-20221231
 # freqtrade backtesting --config user_data/config_GridV6_tmp7.json --strategy GridV6_tmp7 --timerange=20210101-20221231
-# Tested , very good
-# freqtrade backtesting --config user_data/config_ElliotV5HO.json --strategy ElliotV5HO --timerange=20231001-20231130
+# Tested , good
+# freqtrade backtesting --config user_data/config_ElliotV5HO.json --strategy ElliotV5HO --timerange=20230101-20231130
 # Tested , Bad
 # freqtrade backtesting --config user_data/config_MultiOffsetLamboV0.json --strategy MultiOffsetLamboV0 --timerange=20221001-20231130
 # Tested , Fair
