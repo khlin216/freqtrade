83a1ae068cd23019b5eaa2586de1e7d9146ff279
Alex Lin <alexlin216@gmail.com>
2023-12-18 10:37:38 +0800

    dry code linode

---
+++ dry-run_El3v2_tag_cofi_green_2_linode.sh
@@ -0,0 +1,3 @@
+freqtrade trade --config user_data/config_El3v2_tag_cofi_green_2_dry_run_linode.json --strategy EI3v2_tag_cofi_green --db-url sqlite:///El3v2_tag_cofi_green_2.dryrun_linode.sqlite
+
+
