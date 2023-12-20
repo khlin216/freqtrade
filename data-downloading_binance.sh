158e425d9f1fd154a7a3a8e2c5fb64f67d826219
khlin <khlincode@gmail.com>
2023-12-20 12:07:26 +0800

    update for data download

---
+++ data-downloading_binance.sh
@@ -0,0 +1,10 @@
+freqtrade download-data --exchange binance --pairs .*/USDT -t 1m --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 5m --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 15m --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 1h --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 4h --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 8h --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 12h --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 1d --timerange 20180101-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 2h --timerange 201801010-
+freqtrade download-data --exchange binance --pairs .*/USDT -t 3m --timerange 201801010-
---
+++ data-downloading_kucoin.sh
@@ -0,0 +1,3 @@
+freqtrade download-data --exchange kucoin --config user_data/config_kraken.json --pairs .*/USDT -t 5m --timerange 20180101-
+# freqtrade download-data --exchange kucoin --config user_data/config_kraken.json --pairs .*/USDT -t 15m --timerange 20180101-
+freqtrade download-data --exchange kucoin --config user_data/config_kraken.json --pairs .*/USDT -t 1h --timerange 20180101-
