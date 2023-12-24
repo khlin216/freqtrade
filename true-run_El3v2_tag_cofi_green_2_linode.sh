51e2bd56ad374ade86622e98ce51bd8d1f2d93c7
“alexlin” <alexlin216.com>
2023-12-24 14:21:28 +0000

    update for program run

--- true-run_El3v2_tag_cofi_green_2_linode.sh
+++ true-run_El3v2_tag_cofi_green_2_linode.sh
@@ -1,3 +1,1 @@
 freqtrade trade --config user_data/config_El3v2_tag_cofi_green_2_truerun_linode.json --strategy EI3v2_tag_cofi_green --db-url sqlite:///El3v2_tag_cofi_green_2.truerun_linode.sqlite
-
-
