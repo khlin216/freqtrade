1f69286642d9b6e0489e39daa6b72cafba00a40b
khlin <khlincode@gmail.com>
2023-12-25 22:31:10 +0800

    backtesting sh

---
+++ backtesting_kucoin.sh
@@ -0,0 +1,23 @@
+# Tested ,Good
+# freqtrade backtesting --config user_data/config_ClucHAnix_5m_4.json --strategy ClucHAnix_5m --timerange=20210101-20221231
+# Tested ,very Good
+# freqtrade backtesting --config user_data/config_ElliotV5_SMA.json --strategy ElliotV5_SMA --timerange=20231001-20231130
+# Tested , very good
+# freqtrade backtesting --config user_data/config_RalliV1_2.json --strategy RalliV1 --timerange=20230101-20231130
+# Tested , very good
+# freqtrade backtesting --config user_data/config_El3v2_tag_cofi_green_2_bybit.json --strategy EI3v2_tag_cofi_green --timerange=20200101-20231130
+# Tested , very good
+# freqtrade backtesting --config user_data/config_NASOSv7_bybit.json --strategy NASOSv7 --timerange=20230101-20231130
+# Tested , Bad
+# freqtrade backtesting --config user_data/config_min5v5.json --strategy min5v5 --timerange=20210101-20221231
+# freqtrade backtesting --config user_data/config_GridV6_tmp7.json --strategy GridV6_tmp7 --timerange=20210101-20221231
+# Tested , good
+# freqtrade backtesting --config user_data/config_ElliotV5HO.json --strategy ElliotV5HO --timerange=20230101-20231130
+# Tested , Bad
+# freqtrade backtesting --config user_data/config_MultiOffsetLamboV0.json --strategy MultiOffsetLamboV0 --timerange=20221001-20231130
+# Tested , Fair
+# freqtrade backtesting --config user_data/config_NotAnotherSMAOffsetStrategy_2.json --strategy NotAnotherSMAOffsetStrategy --timerange=20220101-20231130
+
+# Tested ,very Good
+freqtrade backtesting --config user_data/config_ElliotV5_SMA_dry_run_kucoin.json --strategy ElliotV5_SMA --timerange=20221001-20231030
+
