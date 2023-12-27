f068abea9ea198c4d03c81a8f557de8bc4b32d83
khlin <khlincode@gmail.com>
2023-12-27 09:11:01 +0800

    add more backtest shell

--- backtesting.sh
+++ backtesting.sh
@@ -10,14 +10,14 @@
 # freqtrade backtesting --config user_data/config_ClucHAnix_5mTB1_3.json --strategy ClucHAnix_5mTB1 --timerange=20210101-20221231
 # Tested ,Good
 # freqtrade backtesting --config user_data/config_ClucHAnix_5m_4.json --strategy ClucHAnix_5m --timerange=20210101-20221231
-# Tested ,very Good
-# freqtrade backtesting --config user_data/config_ElliotV5_SMA.json --strategy ElliotV5_SMA --timerange=20231001-20231130
+# Tested , Good
+# freqtrade backtesting --config user_data/config_ElliotV5_SMA.json --strategy ElliotV5_SMA --timerange=20230101-20231130
 # Tested , very good
 # freqtrade backtesting --config user_data/config_RalliV1_2.json --strategy RalliV1 --timerange=20230101-20231130
 # Tested , very good
-# freqtrade backtesting --config user_data/config_El3v2_tag_cofi_green_2.json --strategy EI3v2_tag_cofi_green --timerange=20200101-20231130
+# freqtrade backtesting --config user_data/config_El3v2_tag_cofi_green_2.json --strategy-path EI3v2_tag_cofi_green_2 --strategy EI3v2_tag_cofi_green --timerange=20230101-20231130
 # Tested , very good
-freqtrade backtesting --config user_data/config_NASOSv7.json --strategy NASOSv7 --timerange=20230101-20231130
+# freqtrade backtesting --config user_data/config_NASOSv7.json --strategy NASOSv7 --timerange=20230101-20231130
 # Tested , Bad
 # freqtrade backtesting --config user_data/config_min5v5.json --strategy min5v5 --timerange=20210101-20221231
 # freqtrade backtesting --config user_data/config_GridV6_tmp7.json --strategy GridV6_tmp7 --timerange=20210101-20221231
@@ -27,4 +27,22 @@ freqtrade backtesting --config user_data/config_NASOSv7.json --strategy NASOSv7 
 # freqtrade backtesting --config user_data/config_MultiOffsetLamboV0.json --strategy MultiOffsetLamboV0 --timerange=20221001-20231130
 # Tested , Fair
 # freqtrade backtesting --config user_data/config_NotAnotherSMAOffsetStrategy_2.json --strategy NotAnotherSMAOffsetStrategy --timerange=20220101-20231130
- 
+# Tested , very good
+# freqtrade backtesting --config user_data/config_El3v2_tag_cofi_green_2.json --strategy-path EI3v2_tag_cofi_green_3474790687 --strategy EI3v2_tag_cofi_green --timerange=20200101-20231130
+# Tested , very good
+# freqtrade backtesting --config user_data/config_El3v2_tag_cofi_green_2.json --strategy-path EI3v2_tag_cofi_green_2 --strategy-list EI3v2_tag_cofi_green EI3v2_tag_cofi_dca_green --timerange=20230101-20231130 --timeframe 5m
+# freqtrade backtesting --config user_data/config_ClucHAnix_5mTB1_3.json --strategy-list ClucHAnix_5mTB1 ClucHAnix_5m1 --timerange=20210101-20221231 --timeframe 5m
+# Tested , very good
+# freqtrade backtesting --config user_data/config_NASOSv7.json --strategy NASOSvSH1 --timerange=20221201-20231130
+# Tested , very good better than NASOSvSH1
+# freqtrade backtesting --config user_data/config_NASOSv7.json --strategy NASOSv4_SMA --timerange=20220101-20231130
+# Tested, Fair
+# freqtrade backtesting --config user_data/config_NASOSv7.json --strategy SMAOG --timerange=20230101-20231130
+# Tested, Fair
+# freqtrade backtesting --config user_data/config_NASOSv7.json --strategy NotAnotherSMAOffsetStrategy --timerange=20230101-20231130
+# freqtrade backtesting --config user_data/config_NASOSv7.json --strategy NostalgiaForInfinityV7 --timerange=20230101-20231130
+# tested very Good
+# freqtrade backtesting --config user_data/config_NASOSv7.json --strategy NostalgiaForInfinityV7_SMAv2_1 --timerange=20220101-20231130
+# tested Good
+# freqtrade backtesting --config user_data/config_NASOSv7.json --strategy SMAOffsetProtectOptV1Mod --timerange=20220101-20231130
+
